333b52adeef6a86c9aabddf3e6f208d22d6a9563
Wahab Jawed <wahabjawed@gmail.com>
2018-02-13 23:56:43 +0500

    repo setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# bmhn
-Build Multi-Host Network - Hyperledger Fabric
+## Build Multi-Host Network (BMHN)
+
+The directions for using this are documented at my article at Medium.
+["Hyperledger Fabric on Multiple Hosts"](https://medium.com/@wahabjawed/hyperledger-fabric-on-multiple-hosts-a33b08ef24f)
---
+++ bmhn.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This script will orchestrate a sample end-to-end execution of the Hyperledger
+# Fabric network.
+#
+# The end-to-end verification provisions a sample Fabric network consisting of
+# two organizations, each maintaining two peers, and a “solo” ordering service.
+#
+# This verification makes use of two fundamental tools, which are necessary to
+# create a functioning transactional network with digital signature validation
+# and access control:
+#
+# * cryptogen - generates the x509 certificates used to identify and
+#   authenticate the various components in the network.
+# * configtxgen - generates the requisite configuration artifacts for orderer
+#   bootstrap and channel creation.
+#
+# Each tool consumes a configuration yaml file, within which we specify the topology
+# of our network (cryptogen) and the location of our certificates for various
+# configuration operations (configtxgen).  Once the tools have been successfully run,
+# we are able to launch our network.  More detail on the tools and the structure of
+# the network will be provided later in this document.  For now, let's get going...
+
+# prepending $PWD/../bin to PATH to ensure we are picking up the correct binaries
+# this may be commented out to resolve installed version of tools if desired
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+
+# Ask user for confirmation to proceed
+function askProceed () {
+  read -p "Continue (y/n)? " ans
+  case "$ans" in
+    y|Y )
+      echo "proceeding ..."
+    ;;
+    n|N )
+      echo "exiting..."
+      exit 1
+    ;;
+    * )
+      echo "invalid response"
+      askProceed
+    ;;
+  esac
+}
+
+
+
+# We will use the cryptogen tool to generate the cryptographic material (x509 certs)
+# for our various network entities.  The certificates are based on a standard PKI
+# implementation where validation is achieved by reaching a common trust anchor.
+#
+# Cryptogen consumes a file - ``crypto-config.yaml`` - that contains the network
+# topology and allows us to generate a library of certificates for both the
+# Organizations and the components that belong to those Organizations.  Each
+# Organization is provisioned a unique root certificate (``ca-cert``), that binds
+# specific components (peers and orderers) to that Org.  Transactions and communications
+# within Fabric are signed by an entity's private key (``keystore``), and then verified
+# by means of a public key (``signcerts``).  You will notice a "count" variable within
+# this file.  We use this to specify the number of peers per Organization; in our
+# case it's two peers per Org.  The rest of this template is extremely
+# self-explanatory.
+#
+# After we run the tool, the certs will be parked in a folder titled ``crypto-config``.
+
+# Generates Org certs using cryptogen tool
+function generateCerts (){
+  which cryptogen
+  if [ "$?" -ne 0 ]; then
+    echo "cryptogen tool not found. exiting"
+    exit 1
+  fi
+  echo
+  echo "##########################################################"
+  echo "##### Generate certificates using cryptogen tool #########"
+  echo "##########################################################"
+  if [ -d "crypto-config" ]; then
+    rm -Rf crypto-config
+  fi
+  cryptogen generate --config=./crypto-config.yaml
+  if [ "$?" -ne 0 ]; then
+    echo "Failed to generate certificates..."
+    exit 1
+  fi
+  echo
+}
+
+# The `configtxgen tool is used to create four artifacts: orderer **bootstrap
+# block**, fabric **channel configuration transaction**, and two **anchor
+# peer transactions** - one for each Peer Org.
+#
+# The orderer block is the genesis block for the ordering service, and the
+# channel transaction file is broadcast to the orderer at channel creation
+# time.  The anchor peer transactions, as the name might suggest, specify each
+# Org's anchor peer on this channel.
+#
+# Configtxgen consumes a file - ``configtx.yaml`` - that contains the definitions
+# for the sample network. There are three members - one Orderer Org (``OrdererOrg``)
+# and two Peer Orgs (``Org1`` & ``Org2``) each managing and maintaining two peer nodes.
+# This file also specifies a consortium - ``SampleConsortium`` - consisting of our
+# two Peer Orgs.  Pay specific attention to the "Profiles" section at the top of
+# this file.  You will notice that we have two unique headers. One for the orderer genesis
+# block - ``TwoOrgsOrdererGenesis`` - and one for our channel - ``TwoOrgsChannel``.
+# These headers are important, as we will pass them in as arguments when we create
+# our artifacts.  This file also contains two additional specifications that are worth
+# noting.  Firstly, we specify the anchor peers for each Peer Org
+# (``peer0.org1.example.com`` & ``peer0.org2.example.com``).  Secondly, we point to
+# the location of the MSP directory for each member, in turn allowing us to store the
+# root certificates for each Org in the orderer genesis block.  This is a critical
+# concept. Now any network entity communicating with the ordering service can have
+# its digital signature verified.
+#
+# This function will generate the crypto material and our four configuration
+# artifacts, and subsequently output these files into the ``channel-artifacts``
+# folder.
+#
+# If you receive the following warning, it can be safely ignored:
+#
+# [bccsp] GetDefault -> WARN 001 Before using BCCSP, please call InitFactories(). Falling back to bootBCCSP.
+#
+# You can ignore the logs regarding intermediate certs, we are not using them in
+# this crypto implementation.
+
+# Generate orderer genesis block, channel configuration transaction and
+# anchor peer update transactions
+function generateChannelArtifacts() {
+  which configtxgen
+  if [ "$?" -ne 0 ]; then
+    echo "configtxgen tool not found. exiting"
+    exit 1
+  fi
+
+  echo "##########################################################"
+  echo "#########  Generating Orderer Genesis block ##############"
+  echo "##########################################################"
+  # Note: For some unknown reason (at least for now) the block file can't be
+  # named orderer.genesis.block or the orderer will fail to launch!
+  configtxgen -profile OneOrgOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+  if [ "$?" -ne 0 ]; then
+    echo "Failed to generate orderer genesis block..."
+    exit 1
+  fi
+  echo
+  echo "#################################################################"
+  echo "### Generating channel configuration transaction 'channel.tx' ###"
+  echo "#################################################################"
+  configtxgen -profile OneOrgChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+  if [ "$?" -ne 0 ]; then
+    echo "Failed to generate channel configuration transaction..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Org1MSP   ##########"
+  echo "#################################################################"
+  configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+  if [ "$?" -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Org1MSP..."
+    exit 1
+  fi
+  echo
+}
+
+# Obtain the OS and Architecture string that will be used to select the correct
+# native binaries for your platform
+OS_ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+# timeout duration - the duration the CLI should wait for a response from
+# another container before giving up
+CLI_TIMEOUT=10
+#default for delay
+CLI_DELAY=3
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
+
+EXPMODE="Generating certs and genesis block for"
+
+# Announce what was requested
+
+echo "${EXPMODE} with channel '${CHANNEL_NAME}' and CLI timeout of '${CLI_TIMEOUT}'"
+
+# ask for confirmation to proceed
+askProceed
+
+# generate crypto-material
+generateCerts
+generateChannelArtifacts
